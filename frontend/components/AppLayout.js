31c42c74f89345217c3137e6b68e3e533191857c
Daniel <dan.sjkim91@gmail.com>
2021-07-08 16:44:29 +0900

    feat: add login form, responsive grid

--- frontend/components/AppLayout.js
+++ frontend/components/AppLayout.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
 import PropTypes from 'prop-types';
 import Link from 'next/link';
-import { Menu } from 'antd';
+import { Menu, Input, Row, Col } from 'antd';
+
+import UserProfile from '../components/UserProfile';
+import LoginForm from '../components/LoginForm';
 
 const AppLayout = ({ children }) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
-        <div>
-            <Menu mode="horizontal">
-                <Menu.Item>
-                    <Link href="/"><a>노드</a></Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/profile"><a>프로필</a></Link>
-                </Menu.Item>
-                <Menu.Item>
-                    <Link href="/signup"><a>회원가입</a></Link>
-                </Menu.Item>
-            </Menu>
+      <div>
+        <Menu mode="horizontal">
+          <Menu.Item>
+            <Link href="/"><a>노드</a></Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/profile"><a>프로필</a></Link>
+          </Menu.Item>
+          <Menu.Item>
+            <Input.Search enterButton style={{ verticalAlign: 'middle' }}/>
+          </Menu.Item>
+          <Menu.Item>
+            <Link href="/signup"><a>회원가입</a></Link>
+          </Menu.Item>
+        </Menu>
+        <Row gutter={8}>
+          <Col xs={24} md={6}>
+            {isLoggedIn ? <UserProfile /> : <LoginForm />}
+          </Col>
+          <Col xs={24} md={12}>
             {children}
-        </div>
+          </Col>
+          <Col xs={24} md={6}>
+            <a href="https://www.kurly.com" target="_blank" rel="noreferrer noopener">Kurly</a>
+          </Col>
+        </Row>
+      </div>
     );
 }
 
--- frontend/pages/signup.js
+++ frontend/pages/signup.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import AppLayout from '../components/AppLayout';
+import Head from 'next/head';
 
 const Signup = () => {
     return (
