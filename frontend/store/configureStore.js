a454d53fb923a16e39e5106902cb99eb7a291e7c
Daniel <dan.sjkim91@gmail.com>
2021-09-06 22:43:40 +0900

    feat: redux middleware, devtools 세팅

--- frontend/store/configureStore.js
+++ frontend/store/configureStore.js
@@ -1,10 +1,15 @@
 import { createWrapper } from 'next-redux-wrapper';
-import { createStore } from 'redux';
-
+import { applyMiddleware, compose, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import reducer from '../reducers'
 
 const configureStore = () => {
-  const store = createStore(reducer);
+  const middlewares = [];
+  const enhancer = process.env.NODE_ENV === 'production'
+    ? compose(applyMiddleware(...middlewares))
+    : composeWithDevTools(applyMiddleware(...middlewares))
+  const store = createStore(reducer, enhancer);
+
   store.dispatch({
     type: 'CHANGE_NICKNAME',
     data: 'seungjune',
@@ -12,6 +17,9 @@ const configureStore = () => {
   return store;
 };
 
-const wrapper = createWrapper(configureStore, { debug: process.env.NODE_ENV === 'development', });
+const wrapper = createWrapper(configureStore, {
+  debug: process.env.NODE_ENV === 'development',
+}
+);
 
 export default wrapper;
